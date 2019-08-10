82417b77fda3fad80c9c66552d0f82428f5600c9
Sars <SarsSappy@gmail.com>
2019-08-10 15:54:38 +0530

    Minimal Improvement
    Improved the privacy of script, hide the password using -s (silent) argument so that no one can see what you typed ;)

--- Login_Script/login.sh
+++ Login_Script/login.sh
@@ -2,19 +2,19 @@
 #Simple login script which asks for the predefined usrnm & pswd
 #it terminates the app if credentials dont match.
 #Its not foolproof anyone with basic linux knowledge can bypass it, just by issuing Cancel command ( ctrl + c ).
-#Version: 0.1 Initial Release
+#Version: 0.2
 
 echo "Username"
 read n
 echo "Password"
-read p
+read -s p
 
-if [[ $n == "Ali" && $p == "Sars" ]]; #enter your credentials here
+if [[ $n == "Ali" && $p == "Sars" ]]; 		#enter your credentials here
 then
 echo "Access Granted!"
 
 else
 echo "Un-Authorized User... Exiting"
 sleep 3
-kill -9 $PPID #kills the parent process id i.e., the app itself
+kill -9 $PPID 			#kills the parent process id i.e., the app itself
 fi
