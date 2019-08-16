78f631f27d9fe4eafe40cbc6d0bcb5c3a4724130
SarsSappy <SarsSappy@gmail.com>
2019-08-16 23:13:14 +0530

    Added Encryption & Decryption
    Finally! It's here, improved the security with encryption. Now the script is way more secure than before but still its not entirely secure.
    Devlogs? Well, wait till the next commit ;)

--- Login_Script/login.sh
+++ Login_Script/login.sh
@@ -5,18 +5,39 @@
 
 #Path where the valuables are stored.
 pt=../usr/share/.login
+#Path for temporarily storing unencrypted valuables.
+opt=../usr/tmp/.tmplog
+
+#Function for encrypting & decrypting cred.
+encrypt() {
+openssl enc -aes-256-cbc -pbkdf2 -salt -in $opt -out $pt -k $pf
+chmod u-rw $pt
+}
+
+decrypt() {
+chmod u+rw $pt
+openssl enc -d -aes-256-cbc -pbkdf2 -salt -in $pt -out $opt -k $pss $old
+}
 
 #Function that sets credentials.
 st_crd() {
 	read -p "Set Your Username: " un
 	read -s -p "Set Your Password: " pf
 
+	#rm the unencrypted cred file if it exists
+	if [ -f $opt ];
+	then
+		rm $opt
+	fi
+
 	if [[ $un != "" && $pf != "" ]];
 	then
 		touch $pt
-	        echo -e "$un\n$pf" >> $pt
+	        echo -e "$un\n$pf" >> $opt
 	        echo -e "\nCredentials added!!"
+		encrypt
 		echo -e "\nExiting Termux, please restart it!"
+		chmod u-rw $pt
 		sleep 3
 		kill -9 $PPID
 	else
@@ -26,15 +47,15 @@ st_crd() {
 
 }
 
-#Function for re-setting credentials
+#Function for re-setting credentials.
 rst_crd() {
-	read -p "Enter Old Usrname OR Password: " old
+	read -p "Enter Old Password: " old
+	decrypt
 
-	#Read the cred.
-	unm=$( head -n 1 $pt )
-	p=$( tail -n 1 $pt )
+	#Read the old pass.
+	p=$( tail -n 1 $opt )
 
-	if [[ $old == $unm || $old == $p ]];
+	if [[ $old == $p ]];
 	then
 		echo -e "\nClearing your Old Credentials..."
 		rm -rf $pt
@@ -44,6 +65,7 @@ rst_crd() {
 	else
 		echo -e "\nOh, you remember your Credentials? :o"
 		echo -e "\nClosing app, re-launch it please"
+		chmod u-rw $pt
 		sleep 3
 		kill -9 $PPID
 	fi
@@ -71,13 +93,14 @@ chk_crd() {
 	trap 'int_trp' INT
 	chmod u+rw $pt
 
-	unm=$( head -n 1 $pt )
-	p=$( tail -n 1 $pt )
-
 	echo "Enter Username"
 	read n
 	echo "Enter Passwd"
 	read -s pss
+	decrypt
+
+	unm=$( head -n 1 $opt )
+	p=$( tail -n 1 $opt )
 
 	if [[ $n == $unm && $pss == $p ]];
 	then
