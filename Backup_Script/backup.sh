f6c566aa08e9999aba697082edc391117b0e6aba
SarsSappy <SarsSappy@gmail.com>
2019-08-20 22:17:49 +0530

    Backup Utility: Minor Improvement
    It works, fine for now :P
    Some the functions will be used in future, as well as i have a reason for including .config file ;)
    
    Bugs:
    None so far :∆
    
    To-Do:
    *Custom Backup Paths. Allow the user to set their own backup paths.
    *List all Backups.
    *Option to remove Backups right from the script.
    *Option to choose names for Backups.
    *Auto-Backups. ( Probably )
    *Push the backups to user's Git! ( Most important feature :D )
    *Notifying users to take backups whenever their configs gets changed. (Not sure :/)
    *Installation script.
    *Code Optimizations.
    *Beautification using tput.
    *Probably more.

--- Backup_Script/backup.sh
+++ Backup_Script/backup.sh
@@ -1,25 +1,82 @@
 #! /bin/bash
 #Script for backing-up and reinstalling configs, .rc files.
-#Its pain in the @$$ to configure your configs, rc files after re-installation of termux, just in case you messed it somehow
-#A very Barebone script which will be improved in the near future.
+#Its pain in the @$$ to configure your configs, rc, aliases etc files after reinstalling Termux.
+#A Barebone script which will be improved in the near future.
 #Written by Sars!
 #0.1
 
-#Define the path to store the config files.
-bpt=~/storage/shared/TermuxBackups/Configs
-#Create the directory & redirect (if any) errors to null.
-mkdir -p $bpt 2>/dev/null
+#GLOBAL VARIABLES
+#Import the config file containing backup path.
+source ~/.Backup_Utility/.config
+
 #Path from where the files needs to be backed up.
 ipt=../usr/etc/
+
 #Move the archive to TermuxStuff
 ball=~/etc.tar.gz
 
-tball_bup() {
-	echo "Creating Tarball....."
+#Choices
+chc="Backup Restore Quit"
+PS3="Choose: "
+
+#######################################
+#Wrapper functions starts from here, will be utilized later in the script.
+
+ls() {
+	command ls -lh
+}
+
+dl() {
+	rm -r
+}
+
+mkdir() {
+	command mkdir -p $dbpt 2>/dev/null
+}
+
+mv() {
+	command mv $ball $dbpt
+}
+#Wrapper functions ends here.
+######################################
+
+#Function for Backing-Up.
+tar_bup() {
+
+	echo "Creating Backup....."
+	mkdir
 	tar -czpf etc.tar.gz $ipt 2>/dev/null
-	mv $ball $bpt
+	mv
 	sleep 1
 	echo
-	echo -e "Configs Backed-Up at \n$bpt"
+	echo -e "Configs Backed-Up at \n$dbpt"
+	
 }
-tball_bup
+
+#Funtcion for Restoring.
+tar_rest() {
+
+	echo "Restoring Backup..."		
+	tar -xzpf $dbpt -C ../
+	sleep 1
+	echo -e "\nConfigs Restored!"
+
+}
+
+#Display the menu
+#COLUMNS=0
+select opt in $chc
+do
+	if [ $opt == 'Quit' ]
+	then
+		break
+
+	elif [ $opt == 'Backup' ]
+	then
+		tar_bup
+
+	elif [ $opt == 'Restore' ]
+	then
+		tar_rest
+	fi
+done
