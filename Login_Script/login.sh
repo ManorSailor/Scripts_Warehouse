57430f823a10e1c1b090d03255e029e83b5210b7
Sars <SarsSappy@gmail.com>
2019-08-30 20:38:59 +0530

    Professionalize code. Minimal code improvements for our old beloved Login script :)

--- Login_Script/login.sh
+++ Login_Script/login.sh
@@ -9,39 +9,47 @@ pt=../usr/share/.login
 #Path for temporarily storing unencrypted valuables.
 opt=../usr/tmp/.tmplog
 
+#Helper function.
+err_die() {
+	echo -e "\nExiting..."
+	chmod u-rw "$pt"
+	rm "$opt" 2>/dev/null
+	sleep 3
+	kill -9 $PPID
+}
+
 #Function for encrypting & decrypting cred.
 encrypt() {
-openssl enc -aes-256-cbc -pbkdf2 -salt -in $opt -out $pt -k $pf 2>/dev/null #Redirects error if any to /dev/null. Thanks to https://stackoverflow.com/a/25348167
-chmod u-rw $pt
+openssl enc -aes-256-cbc -pbkdf2 -salt -in "$opt" -out "$pt" -k "$1" 2>/dev/null #Redirects error if any to /dev/null. Thanks to https://stackoverflow.com/a/25348167
+chmod u-rw "$pt"
 }
 
 decrypt() {
-chmod u+rw $pt
-openssl enc -d -aes-256-cbc -pbkdf2 -salt -in $pt -out $opt -k $pss 2>/dev/null #Redirects bad-decrypts error if any to /dev/null. Thanks to https://stackoverflow.com/a/25348167
+chmod u+rw "$pt"
+openssl enc -d -aes-256-cbc -pbkdf2 -salt -in "$pt" -out "$opt" -k "$1" 2>/dev/null #Redirects bad-decrypts error if any to /dev/null. Thanks to https://stackoverflow.com/a/25348167
 }
 
 #Function that sets credentials.
 st_crd() {
 	read -p "Set Your Username: " un
-	read -s -p "Set Your Password: " pf
+	read -sp "Set Your Password: " pf
 
 	#rm the unencrypted cred file if it exists
-	if [ -f $opt ];
+	if [ -f "$opt" ];
 	then
-		rm $opt
+		rm "$opt"
 	fi
 
-	if [[ $un != "" && $pf != "" ]];
+	if [[ -n "$un" && -n "$pf" ]];
 	then
-		touch $pt
-	        echo -e "$un\n$pf" >> $opt
-	        echo -e "\nCredentials added!!"
-		encrypt
-		echo -e "\nExiting Termux, please restart it!"
-		sleep 3
-		kill -9 $PPID
+		touch "$pt"
+		echo -e "$un\n$pf" >> "$opt"
+		echo -e "\nCredentials added!!"
+		encrypt "$pf"
+		err_die
 	else
-		echo -e '\nCredentials cannot be empty!'
+		echo
+		echo -e "\nCredentials cannot be empty!"
 		exit
 	fi
 
@@ -49,76 +57,68 @@ st_crd() {
 
 #Function for re-setting credentials.
 rst_crd() {
-	read -p "Enter Old Password: " pss
-	decrypt
+	read -sp "Enter Old Password: " pss
+	decrypt "$pss"
 
 	#Read the old pass.
-	op=$( tail -n 1 $opt 2>/dev/null )
+	op=$( tail -n 1 "$opt" 2>/dev/null )
 
-	if [[ $pss == "$op" && $pss != "" ]];
+	if [[ "$pss" == "$op" && ! -z "$pss" ]];
 	then
 		echo -e "\nClearing your Old Credentials..."
-		rm -rf $pt
+		rm -rf "$pt"
 		echo -e "\nCleared! Please add your new credentials"
 		st_crd
 
 	else
-		echo -e "\nOh, you remember your Credentials? :o"
-		echo -e "\nClosing app, re-launch it please"
-		chmod u-rw $pt
-		sleep 3
-		kill -9 $PPID
+		err_die
 	fi
 }
 
 #Trap the intruder & check if user wants to reset Credentials ;)
 int_trp() {
+	echo
 	echo -e "\nForgot your Credentials?"
 	read ans
 
-	if [[ $ans == "yes" || $ans == "y" || $ans == "Y" || $ans == "Yes" ]];
+	if [[ "$ans" == "yes" || "$ans" == "y" || "$ans" == "Y" || "$ans" == "Yes" ]];
 	then
 		rst_crd
 
 	else
-		echo "Intruder Alert!!"
-		chmod u-rw $pt
-		sleep 3
-		kill -9 $PPID
+		err_die
 	fi
 }
 
 #Function for checking if the user is authorized.
 chk_crd() {
 	trap 'int_trp' INT
-	chmod u+rw $pt
+	chmod u+rw "$pt"
 
-	echo "Enter Username"
-	read n
-	echo "Enter Passwd"
-	read -s pss
-	decrypt
+	read -p "Enter Username: " n
+	read -sp "Enter Password: " pss
+	decrypt "$pss"
 
-	unm=$( head -n 1 $opt 2>/dev/null )
-	p=$( tail -n 1 $opt 2>/dev/null )
+	unm=$( head -n 1 "$opt" 2>/dev/null )
+	p=$( tail -n 1 "$opt" 2>/dev/null )
 
-	if [[ $n == "" || $pss == "" ]];
+	if [[ -z "$n" || -z "$pss" ]];
 	then
-		echo "Please Enter your Credentials... Exiting"
-		sleep 3
-		kill -9 $PPID
+		echo
+		echo
+		echo "Enter your Credentials!!"
+		err_die
 
-	elif [[ $n == "$unm" && $pss == "$p" ]];
+	elif [[ "$n" == "$unm" && "$pss" == "$p" ]];
 	then
-		echo "Welcome!"
+		echo
+		echo -e "\nWelcome!"
 
 	else
-		echo "Un-Authorized User ..exiting"
-		sleep 3
-		kill -9 $PPID
+		echo
+		echo -e "\nUn-Authorized User!"
+		err_die
 	fi
-	chmod u-rw $pt
-	rm $opt
 }
 
 #Checks if the user is running the script for first time, it's not the proper way of doing this i guess but for now it works :P
