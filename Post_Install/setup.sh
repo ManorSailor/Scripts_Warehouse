5a49e135c632baf65062d5e80d6ddc8b3f55bfed
Sars <SarsSappy@gmail.com>
2019-09-14 20:54:19 +0530

    help() function completed... for now. Strengthening the base of script so as to ensure easier future improvements.

--- Post_Install/setup.sh
+++ Post_Install/setup.sh
@@ -9,9 +9,31 @@
 source .setup/.config
 source .setup/packages
 
+#Helper Functions.
 spk() {
 	echo -e "\n$1"
 }
+
+bl() {
+	tput bold
+}
+
+ul() {
+	tput smul
+}
+
+rul() {
+	tput rmul
+}
+
+rst() {
+	tput sgr0
+}
+
+clr() {
+	tput clear
+}
+#Ends.
 
 #Functions helpful in Debugging.
 err_die() {
@@ -29,12 +51,21 @@ fail() {
 #Ends
 
 help() {
-	echo "setup {TYPE}... {FLAG}"
-	spk "E.G, setup full or setup minimal -S"
-	echo "If no TYPE is defined script will continue with DEFAULT "
-			tput bold
-			spk "TYPES..."
-			tput sgr0
-	echo "FULL: Installs every packages "
-	echo "Default"
-}
+	clr
+	bl
+	echo "$(ul)SYNTAX $(rul)"
+	echo "       setup {TYPE}... {FLAG}"
+
+			spk "$(ul)TYPES $(rul)"
+			rst
+			echo "If no TYPE is defined script will first look for user defined packages list or else it will continue with DEFAULT."
+			spk "       -D, --default     Installs the packages which comes predefined                          in packages list."
+			spk "       -M, --minimal     Installs the basic packages needed to make                            Termux feel more Linux like."
+			spk "$(bl) E.g, setup -D or setup -M $(rst)"
+
+	spk "$(bl)$(ul)FLAGS $(rul)$(rst)"
+			echo "Flags are optional, as of now only SILENT flag is available."
+			spk "       -s, --silent      Makes the script run Silently."
+	spk "That's all the help available.. for now"
+} 
+help
